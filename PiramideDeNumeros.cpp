c84cbc9ac49b8f6d89f5a5d66203c6ea1002619e
CrazzyCoderHacker <142330384+CrazzyCoderHacker@users.noreply.github.com>
2023-10-03 10:23:37 -0600

    Add files via upload

---
+++ PiramideDeNumeros.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) 
+{
+	int i;
+	int j;
+	int grande;
+	
+    printf("De cuanto quieres tu piramide: ");
+    scanf("%d", &grande);
+
+    for (i = 1; i <= grande; i++) 
+	{
+        for (j = 1; j <= i; j++) 
+		{
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+	return 0;
+}
