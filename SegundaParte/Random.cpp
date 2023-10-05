d06022768df3d00391f88a182ef4e489b8e0a7ad
CrazzyCoderHacker <142330384+CrazzyCoderHacker@users.noreply.github.com>
2023-10-05 09:08:42 -0600

    Random.cpp

--- SegundaParte/Random.cpp
+++ SegundaParte/Random.cpp
@@ -8,6 +8,7 @@
 int main(int argc, char** argv) 
 {
  int i, x, y, t;
+	float p, q, m;
 
     srand(time(NULL));
     x = rand() % 5 + 1;
@@ -37,15 +38,15 @@ int main(int argc, char** argv)
 		        } 
 					else if (x == 4) 
 					{
-			            i = rand() % 9 + 1;
-			            y = rand() % 9 + 1;
-			            t = i / y;
-			            printf("%d / %d = %d\n", i, y, t);
+			            p = rand() % 9 + 1;
+			            q = rand() % 9 + 1;
+			            m = p / q;
+			            printf("%f / %f = %f\n", p, q, m);
 			        }
 
         x = rand() % 5 + 1;
     }
 
-    printf("Se acabo estimado\n");	
+    printf("Se acabo estimado\n");		
 	return 0;
 }
